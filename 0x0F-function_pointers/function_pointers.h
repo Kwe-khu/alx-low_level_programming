663d61b615ad9b0d4a92e9a92da79334b34a1fec
myusername <myemail@gmail.com>
2023-03-20 02:35:04 -0700

    Task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,4 +5,4 @@ int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
-#end
+#endif
